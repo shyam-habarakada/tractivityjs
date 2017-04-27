efbb99b548bb921ff034fd4984c47d1deffd6f09
Shyam Habarakada <shyam.habarakada@gmail.com>
2017-04-27 08:43:17 -0700

    track mouseover and mousedown

--- lib.js
+++ lib.js
@@ -3,7 +3,7 @@
   // Template forked from underscorejs.
   // See https://github.com/jashkenas/underscore/blob/master/underscore.js
 
-  "use strict";
+  'use strict';
 
   // window, in the browser. no server support.
   var root = this;
@@ -11,8 +11,10 @@
   // TODO conflict detection
   var previousTractivity = root.Tractivity;
 
+  var logging = false;
+
   var log = function(msg) {
-    console.log('[tractivity] ' + msg);
+    logging && console.log('[tractivity] ' + msg);
   };
 
   // Create a safe reference to the Tracktivity object for use below.
@@ -26,8 +28,50 @@
 
   tractivity.VERSION = '0.1.0';
 
+  tractivity.enableLogging = function(enable) {
+    logging = enable;
+  };
+
   tractivity.enable = function() {
     log('enabled');
+
+    var getElementInfo = function(el) {
+      var s;
+      if(el) {
+        s = el.nodeName;
+        if(el.dataset.trackedId) {
+          s = s + ' id=' + el.dataset.trackedId;
+        } else {
+          if(el.id) {
+            s = s + ' id=' + el.id;
+          }
+        };
+      } else {
+        s = 'unknown';
+      }
+      return s;
+    };
+
+    var onMouseOver = function(e) {
+      var el = e.target,
+          elInfo;
+      if(el.dataset.tracked === 'true') {
+        elInfo = getElementInfo(e.target);
+        log('mouse moved over element ' + elInfo + ' (' + e.screenX + ',' + e.screenY + ')');
+      }
+    };
+
+    var onMouseDown = function(e) {
+      var el = e.target,
+          elInfo;
+      if(el.dataset.tracked === 'true') {
+        elInfo = getElementInfo(e.target);
+        log('mouse down on element ' + elInfo + ' (' + e.screenX + ',' + e.screenY + ')');
+      }
+    }
+
+    document.addEventListener('mouseover', onMouseOver);
+    document.addEventListener('mousedown', onMouseDown);
   }
 
 }.call(this));
